439e9f9e2dbb8d428c454d47d8b301565d141afd
mayli10 <mali@vassar.edu>
2018-01-12 18:59:44 -0800

    table showing percentage of values showing up in a feature
    below the comment line is the code tos how these values. above the comment line reflects my experimenting with storing values in vectors or lists

--- model.R
+++ model.R
@@ -1,5 +1,29 @@
-adult.data <- read.csv('adult-data.csv')
-plot(adult.data$gender)
-plot(adult.data$has.autism.correct.response)
-plot(adult.data$country.of.residence)
 install.packages("e1071")
+install.packages("caTools")
+adult.data <- read.csv('adult-data.csv', stringsAsFactors = FALSE)
+plot(adult.data$gender)
+library(e1071)
+library(caTools)
+
+first = c(unname(adult.data[1,])) #output is what you i sent you, and the list of 21
+length(first) #output is 21 (which is correct)
+is.vector(first) #output is true
+first
+
+
+c(1,1,1,1,1,0,0,1,1,0,0,26,"f","White-European","no","no","United States","no",6,18,"and","more","Self","NO")
+
+
+c(adult.data[1,])
+unname(c(adult.data[1,]))
+mouse
+orange <- c(1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,'aefa',2,3,1,'faa',3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3)
+length(orange)
+length(mouse)
+
+########################
+str(adult.data)
+round(prop.table(table(adult.data$a1.score))*100, digits = 1)
+adult.data$a1.score = factor(adult.data$a1.score)
+
+
